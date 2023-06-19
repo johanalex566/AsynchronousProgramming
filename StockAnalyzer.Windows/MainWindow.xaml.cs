29e1fbc18ddf006f0d713990819123507a3a711a
johanalex566 <johanalex566@gmail.com>
2023-06-19 17:50:24 -0500

    Asynchronous streams foreach concurrently the hand of the users

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using StockAnalyzer.Core;
 using StockAnalyzer.Core.Domain;
 using StockAnalyzer.Core.Services;
+using StockAnalyzer.Windows.Services;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -24,74 +26,32 @@ public partial class MainWindow : Window
         InitializeComponent();
     }
 
-    CancellationTokenSource? cancellationTokenSource;
+
 
     private async void Search_Click(object sender, RoutedEventArgs e)
     {
 
-        if (cancellationTokenSource is not null)
-        {
-            //Already hace an instance of the cancellation token source?
-            //This means the button has already been pressed!
-            cancellationTokenSource.Cancel();
-            cancellationTokenSource.Dispose();
-            cancellationTokenSource = null;
-
-            Search.Content = "Search";
-            return;
-        }
-
         try
         {
-            cancellationTokenSource = new();
-
-            Search.Content = "Cancel";
-
             BeforeLoadingStockData();
-            var service = new StockService();
 
-            var identifiers = StockIdentifier.Text.Split(',', ' ');
+            var identifier = StockIdentifier.Text.Split(' ', ',');
 
-            var loadingTasks = new List<Task<IEnumerable<StockPrice>>>();
-            var stocks = new ConcurrentBag<StockPrice>();
+            var data = new ObservableCollection<StockPrice>();
 
-            foreach (var identifier in identifiers)
+            Stocks.ItemsSource = data;
+
+            var service = new StockDiskStreamService();
+
+            var enumerator =  service.GetAllStockPrices();
+
+            await foreach (var price in enumerator.WithCancellation(CancellationToken.None))
             {
-                var loadTask = service.GetStockPricesFor(identifier, cancellationTokenSource.Token);
-
-
-                loadTask = loadTask.ContinueWith(t =>
+                if (identifier.Contains(price.Identifier))
                 {
-                    var aFewStocks = t.Result.Take(5);
-
-                    foreach (var stock in aFewStocks)
-                    {
-                        stocks.Add(stock);
-                    }
-
-                    Dispatcher.Invoke(() =>
-                    {
-                           Stocks.ItemsSource = stocks.ToArray();
-                    });
-
-                    return aFewStocks;
-                });
-
-                loadingTasks.Add(loadTask);
+                    data.Add(price);
+                }
             }
-
-            var timeout = Task.Delay(10000);
-
-            var allStocksLoadingTask = Task.WhenAll(loadingTasks);
-
-            var completedTask = await Task.WhenAny(timeout, allStocksLoadingTask);
-
-            if (completedTask == timeout)
-            {
-                cancellationTokenSource.Cancel();
-                throw new OperationCanceledException("Timeout!");
-            }
-
 
         }
         catch (Exception ex)
@@ -101,10 +61,8 @@ public partial class MainWindow : Window
         finally
         {
             AfterLoadingStockData();
-            cancellationTokenSource?.Dispose();
-            cancellationTokenSource = null;
-            Search.Content = "Search";
         }
+
     }
 
     private static Task<string[]> SearchForStocks(CancellationToken cancellationToken)
